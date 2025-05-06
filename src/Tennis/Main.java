48699b8888c7409047921e598f31881eaa5d0da8
martinva99 <supermartinva99@gmail.com>
2025-05-06 13:09:32 +0200

    Cambios Rama

--- src/Tennis/Main.java
+++ src/Tennis/Main.java
@@ -8,8 +8,8 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome TO TENNIS GAME!"+"\n");
 
-        TennisGame partido1 = new TennisGameV2("Jugador 1", "Jugador 2");
-        partido1.ganarPunto("Jugador 1");
+        TennisGame partido1 = new TennisGameV2("Jugador rama", "Jugador rama2");
+        partido1.ganarPunto("Jugador rama");
 
         System.out.println(partido1.getScore(""));
 
